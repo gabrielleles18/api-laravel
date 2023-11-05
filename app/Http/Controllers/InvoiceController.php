090fe5927f45e80ea74116eb8a93252522aa3248
gabrielleles18 <gabriel.leles18@gmail.com>
2023-11-05 13:57:46 -0300

    Corrige indentação

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class InvoiceController extends Controller
-{
+class InvoiceController extends Controller {
     //
 }
--- database/migrations/2023_11_05_164440_create_invoices_table.php
+++ database/migrations/2023_11_05_164440_create_invoices_table.php
@@ -4,15 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateInvoicesTable extends Migration
-{
+class CreateInvoicesTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
@@ -24,8 +22,7 @@ class CreateInvoicesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('invoices');
     }
 }
